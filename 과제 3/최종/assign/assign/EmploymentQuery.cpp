c013d5544f4a47695eb962374ed4ed91623cfb33
심현섭 <89609620+SHIMspacethere@users.noreply.github.com>
2023-05-24 22:21:07 +0900

    Update EmploymentQuery.cpp

--- 과제 3/최종/assign/assign/EmploymentQuery.cpp
+++ 과제 3/최종/assign/assign/EmploymentQuery.cpp
@@ -1,5 +1,11 @@
 #include "EmploymentQuery.h"
 
+/*
+    함수 이름 : EmploymentQuery::searchInfo
+    기능 : EmploymentList로부터 데이터를 호출하여 id가 같은 경우 emp에 반환한다.
+    전달 인자 : 없음
+    반환 인자 : emp
+*/
 vector<EmploymentList>  EmploymentQuery::searchInfo(){
         vector<EmploymentList> emp;
         vector<EmploymentList>* getList = EmploymentList::getEmploymentList();
@@ -10,6 +16,13 @@ vector<EmploymentList>  EmploymentQuery::searchInfo(){
         }
         return emp;       
 }
+
+/*
+    함수 이름 : EmploymentQueryUI::searchInfo
+    기능 : 업무, 인원 수, 신청 마감일의 정보를 출력한다.
+    전달 인자 : 없음
+    반환 인자 : 없음
+*/
 void EmploymentQueryUI::serachInfo(){ 
         vector<EmploymentList> ret = employmentQuery->searchInfo();
         fprintf(out_fp,"3.2. 등록된 채용 정보 조회\n");
