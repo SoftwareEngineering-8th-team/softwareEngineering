99e7907577e49f2427bd1a6aaa10ff38936bd0c6
심현섭 <89609620+SHIMspacethere@users.noreply.github.com>
2023-05-25 20:52:12 +0900

    안된 것들 추가

--- 과제 3/최종/assign/assign/CompanySignUp.h
+++ 과제 3/최종/assign/assign/CompanySignUp.h
@@ -4,15 +4,23 @@
 #ifndef __COMPANYSIGNUP__
 #define __COMPANYSIGNUP__
 using namespace std;
+
+/*
+	클래스 이름 : CompanySignUp
+	기능 : 회사 회원이 로그인 정보를 등록하는 클래스
+*/
 class CompanySignUp{
 public:
     CompanyMember newMember(string name, int busiNum, string id, string password);
 };
 
+/*
+	클래스 이름 : CompanySignUpUI
+	기능 : 회사 회원이 로그인 정보를 등록하는 UI를 나타내는 클래스
+*/
 class CompanySignUpUI{
 private:
     CompanySignUp* signup = new CompanySignUp();
-
 public:
     void signUp(string name, int busiNum, string id, string password);
 };
--- 과제 3/최종/assign/assign/CompanyStatisticQuery.h
+++ 과제 3/최종/assign/assign/CompanyStatisticQuery.h
@@ -7,11 +7,20 @@
 #ifndef __COMPANYSTATISTICQUERY__
 #define __COMPANYSTATISTICQUERY__
 using namespace std;
+
+/*
+	클래스 이름 : CompanyStatisticQuery
+	기능 : 회사 통계 정보를 출력하는 클래스
+*/
 class CompanyStatisticQuery{
 public:
     unordered_map<string, int> calculateStatistic();
 };
 
+/*
+	클래스 이름 : CompanyStatisticQueryUI
+	기능 : 회사 통계 정보를 받아 UI로 출력하는 클래스
+*/
 class CompanyStatisticQueryUI{
 private:
     CompanyStatisticQuery* companyStatisticQuery = new CompanyStatisticQuery();
